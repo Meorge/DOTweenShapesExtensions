4e1b78471ec290d4ba40a00fd4322a1e3ac864bd
Malcolm Anderson <malcolminyo@gmail.com>
2020-09-16 18:11:20 -0700

    cool I can modify it from here

--- DOTweenShapesExtensionsDisc.cs
+++ DOTweenShapesExtensionsDisc.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 
 public static class DOTweenShapesExtensionsDisc
 {
+    // a comment
     public static Tween DORadius(this Disc disc, float to, float duration) {
         return DOTween.To(() => disc.Radius, radius => disc.Radius = radius, to, duration);
     }
