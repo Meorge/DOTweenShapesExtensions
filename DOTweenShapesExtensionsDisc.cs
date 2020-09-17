60e43f6b5c704c966a160c167c35cacb524e3a01
Malcolm Anderson <malcolminyo@gmail.com>
2020-09-16 17:56:18 -0700

    Initial commit

---
+++ DOTweenShapesExtensionsDisc.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Shapes;
+using DG.Tweening;
+
+public static class DOTweenShapesExtensionsDisc
+{
+    public static Tween DORadius(this Disc disc, float to, float duration) {
+        return DOTween.To(() => disc.Radius, radius => disc.Radius = radius, to, duration);
+    }
+}
